08e0e899b2df20400ee9af5d9c5ab2f7d5af219e
Watson Boyett <watsonboyett@gmail.com>
2016-01-21 00:47:08 -0600

    Total revamp: now using circular buffers to generate new colors as existing ones vanish.

--- BandanaSnake/BandanaSnake.ino
+++ BandanaSnake/BandanaSnake.ino
@@ -2,7 +2,7 @@
 
 #define HEARTBEAT_PIN (13)
 #define NEOPIXEL_PIN (6)
-#define NUM_PIXELS (60)    // 240
+#define NUM_PIXELS (60)
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -21,75 +21,83 @@ void loop()
   snake();
 }
 
-int delayval = 50;
-const float DIM = 0.6;
+
+const int NUM_SNAKES = 3;
+const int SNAKE_LENGTH = NUM_PIXELS / (NUM_SNAKES);
+
 const float FADE_RATE = 0.6;
-const int MIN_DC = 10;
+const float FADE_CUTOFF = 0.05;
+const int FADE_COUNT = ceil(log(FADE_CUTOFF) / log(FADE_RATE));
+const int MIN_DC = 50;
 const int MAX_DC = 250;
+int LOOP_DELAY_ms = 70;
 
+// use circular buffers to display pixel RGB values
+int pixels_r[NUM_PIXELS] = {0};
+int pixels_g[NUM_PIXELS] = {0};
+int pixels_b[NUM_PIXELS] = {0};
 
-int clr_r = MIN_DC;
-int step_r = 29;
-int clr_g = MIN_DC;
-int step_g = 19;
-int clr_b = MIN_DC;
-int step_b = 13;
+int get_buffer_index(int desired_pixel)
+{
+  int index;
+  if (desired_pixel < 0)
+  {
+    index = NUM_PIXELS + desired_pixel;
+  }
+  else if (desired_pixel >= NUM_PIXELS)
+  {
+    index = desired_pixel % NUM_PIXELS;
+  }
+  else
+  {
+    index = desired_pixel;
+  }
+  return index;
+}
+
+int buffer_start = 0;
+int r, g, b = 0;
+float fade_cur = 1;
 void snake()
 {
-  int fence_count = 3;
-  int fence_skip = NUM_PIXELS / fence_count;
-
-  clr_r = (clr_r + step_r);
-  if (clr_r >= MAX_DC || clr_r <= MIN_DC)
+  // determine if buffer start has passed the head of a snake
+  int buffer_start_prev = (buffer_start - 1);
+  bool new_color = (buffer_start > 0) && (buffer_start_prev % SNAKE_LENGTH) == 0;
+  bool fade_tail = (buffer_start > 0) && (buffer_start_prev % SNAKE_LENGTH) < FADE_COUNT;
+  if (new_color || fade_tail)
   {
-    clr_r = constrain(clr_r, MIN_DC, MAX_DC);
-    step_r = -step_r;
-  }
-
-  clr_g = (clr_g + step_g);
-  if (clr_g >= MAX_DC || clr_g <= MIN_DC)
-  {
-    clr_g = constrain(clr_g, MIN_DC, MAX_DC);
-    step_g = -step_g;
-  }
-
-  clr_b = (clr_b + step_b);
-  if (clr_b >= MAX_DC || clr_b <= MIN_DC)
-  {
-    clr_b = constrain(clr_b, MIN_DC, MAX_DC);
-    step_b = -step_b;
-  }
-  clr_b = (clr_b + step_b);
-
-  for (int i = 0; i < fence_skip; i++)
-  {
-
-    for (int j = 0; j < fence_count + 1; j++)
+    // if we've passed the head, then generate a new color
+    if (new_color)
     {
-      float fade_cur = DIM;
-      int px_key = j * fence_skip + i;
-
-      for (int k = 0; k < fence_skip; k++)
-      {
-        int px_cur = px_key - k;
-        if (px_cur < 0)
-        {
-          px_cur = NUM_PIXELS - k;
-        }
-
-        int r, g, b;
-        r = clr_r * fade_cur;
-        g = clr_g * fade_cur;
-        b = clr_b * fade_cur;
-
-        pixels.setPixelColor(px_cur, pixels.Color(r, g, b));
-
-        fade_cur = fade_cur * FADE_RATE;
-      }
+      r = random(10, 100);
+      g = random(MIN_DC, MAX_DC);
+      b = random(MIN_DC, MAX_DC);
+      fade_cur = 1;
     }
 
-    pixels.show();
-    delay(delayval);
+    // fade the new color as we continue to pass through the snake
+    if (fade_tail)
+    {
+      pixels_r[buffer_start_prev] = r * fade_cur;
+      pixels_g[buffer_start_prev] = g * fade_cur;
+      pixels_b[buffer_start_prev] = b * fade_cur;
+      fade_cur = fade_cur * FADE_RATE;
+    }
   }
+
+  // "render" all pixels to "screen"
+  for (int i = 0; i < NUM_PIXELS; i++)
+  {
+    // use buffer offset (rather than shifting arrays)
+    int index = get_buffer_index(buffer_start + i);
+    int r = pixels_r[index];
+    int g = pixels_g[index];
+    int b = pixels_b[index];
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
+  }
+
+  pixels.show();
+  delay(LOOP_DELAY_ms);
+  buffer_start = (buffer_start + 1) % NUM_PIXELS;
 }
 
