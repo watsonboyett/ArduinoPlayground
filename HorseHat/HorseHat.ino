c302d07491d29a13d8e10887a34503a4e17685df
Watson Boyett <watsonboyett@gmail.com>
2017-06-08 11:56:05 -0500

    Updated timing values

--- HorseHat/HorseHat.ino
+++ HorseHat/HorseHat.ino
@@ -1,12 +1,12 @@
 const int SERVO_PIN = 2; 
  
 void setup() {
-  pinMode(s, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
 }
  
-const int PULSE_PERIOD_us = 20000;
+const int PULSE_PERIOD_us = 30000;
 void loop() {
-  int high_time_us = 1550;
+  int high_time_us = 1400;
   digitalWrite(SERVO_PIN, 1);
   delayMicroseconds(high_time_us);
 
